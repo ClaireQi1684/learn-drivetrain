f77b658d7a9b3b2fbaeadbe78ad8bb352b700290
Claire Qi <clairelvbunny@gmail.com>
2024-10-22 17:05:52 -0700

    updated shuffleboard

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,6 +12,8 @@ import com.ctre.phoenix6.hardware.core.CoreTalonFX;
 import com.ctre.phoenix6.signals.ControlModeValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
@@ -22,6 +24,7 @@ public class Drivetrain extends SubsystemBase {
   private TalonFX m_leftSecondary = new TalonFX(DriveConstants.kLeftSecondaryID);
   private Follower m_leftFollower = new Follower(DriveConstants.kLeftPrimaryID, DriveConstants.kLeftOpposeMasterDirection);
   private Follower m_rightFollower = new Follower(DriveConstants.kRightPrimaryID, DriveConstants.kRightOpposeMasterDirection);
+  private final Field2d m_field = new Field2d();
   /** Creates a new Drivetrain. */
   public Drivetrain() {
     m_leftSecondary.setControl(m_leftFollower);
@@ -46,5 +49,55 @@ public class Drivetrain extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    Shuffleboard.getTab("Elevator")
+      .add("Encoder Position", 5);
+    Shuffleboard.getTab("Elevator")
+      .add("Top Limit Switch", true);
+    Shuffleboard.getTab("Elevator")
+      .add("Bottom Limit Switch", false);
+    Shuffleboard.getTab("Intake")
+      .add("Motor Temperature", 45);
+    Shuffleboard.getTab("Intake")
+      .add("Output Current", 23);
+    Shuffleboard.getTab("Intake")
+      .add("Limit Switch", true);
+    Shuffleboard.getTab("Pivot")
+      .add("Motor Current x4 motors", 34);
+    Shuffleboard.getTab("Pivot")
+      .add("Forward Limit Switch", false);
+    Shuffleboard.getTab("Pivot")
+      .add("Backward Limit Switch", false);
+    Shuffleboard.getTab("Shooter")
+      .add("Motor Temperature", 65);
+    Shuffleboard.getTab("Shooter")
+      .add("Motor Current", 54);
+    Shuffleboard.getTab("Swerve")
+      .add("Wheel Angles x4", 12);
+    Shuffleboard.getTab("Swerve")
+      .add("Motor Total Current", 32);
+    Shuffleboard.getTab("Swerve")
+      .add("Pose X", 3.4);
+    Shuffleboard.getTab("Swerve")
+      .add("Pose Y", 2.4);
+    Shuffleboard.getTab("Swerve")
+      .add("Field", m_field);
+    Shuffleboard.getTab("Swerve")
+      .add("Drive velocity x4 motors", 35);
+    Shuffleboard.getTab("Swerve")
+      .add("Drive motor position x4 motors", 3);
+    Shuffleboard.getTab("Swerve")
+      .add("Turn motor position x4", 56);
+    Shuffleboard.getTab("Swerve")
+      .add("Reset encoders button", false);
+    Shuffleboard.getTab("Swerve")
+      .add("Reset heading button", true);
+    Shuffleboard.getTab("Wrist")
+      .add("Encoder Position", 34.5);
+    Shuffleboard.getTab("Wrist")
+      .add("Forward Limit Switch", false);
+    Shuffleboard.getTab("Wrist")
+      .add("Backward Limit Switch", true);
+    Shuffleboard.getTab("Wrist")
+      .add("Motor Current", 23.4);
   }
 }
